be9c5f907e173b660dcd1d5576ddf10a4f9b5a64
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-06-19 03:15:39 +0500

    removed unused imports

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Image from "next/image";
-import Button from "./Button";
 import OptionList from "./OptionList";
 import HeroIntro from "./HeroIntro";
 import OverlayImage from "./OverlayImage";
