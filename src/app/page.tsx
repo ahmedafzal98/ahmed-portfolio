b151293a2f1a05ccbaa5851bf7440f88c8af06d4
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2025-06-26 00:06:10 +0500

    fixed children

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import HeroSection from "@/components/HeroSection";
+import WhatIDoSection from "@/components/WhatIDoSection";
+import TechStackSection from "@/components/TechStackSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import ContactForm from "@/components/ContactForm";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* <Navbar /> */}
-    </div>
+    <>
+      <HeroSection />
+      <WhatIDoSection />
+      <TechStackSection />
+      <PortfolioSection />
+      <ContactForm />
+      <Footer />
+    </>
   );
 }
